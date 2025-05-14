9dbd9d455e71a2f04a750aa18d8ed76789f52baa
willy0483 <willyjensen251@gmail.com>
2025-05-14 10:48:21 +0200

    Jwt Token #6

--- apps/backend/src/auth/auth.service.ts
+++ apps/backend/src/auth/auth.service.ts
@@ -2,10 +2,16 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { SignInInput } from './dto/signin.input';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { verify } from 'argon2';
+import { JwtService } from '@nestjs/jwt';
+import { AuthJwtPayload } from './types/auth-jwtPayload';
+import { User } from '@prisma/client';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+  ) {}
   async validatelocalUser({ email, password }: SignInInput) {
     const user = await this.prisma.user.findUnique({
       where: {
@@ -21,5 +27,21 @@ export class AuthService {
       throw new UnauthorizedException('Invalid Credentials!');
 
     return user;
+  }
+
+  async genreateToken(userId: number) {
+    const payload: AuthJwtPayload = { sub: userId };
+    const accessToken = await this.jwtService.signAsync(payload);
+    return { accessToken };
+  }
+
+  async login(user: User) {
+    const { accessToken } = await this.genreateToken(user.id);
+    return {
+      id: user.id,
+      name: user.name,
+      avatar: user.avatar,
+      accessToken,
+    };
   }
 }
